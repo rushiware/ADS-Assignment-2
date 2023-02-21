2f6f47fb9d230aab20396af54e5b2d498e40b579
rushiware <85820996+rushiware@users.noreply.github.com>
2023-02-21 11:34:35 +0530

    Add files via upload

---
+++ src/component/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+export default function Home() {
+
+  return (document.title = "Student Management System",
+    <div className='text-center'>
+        <h1 className='text-center mt-4'>Welcome to </h1>
+        <h1 className='text-center mt-4'>Student Management System</h1>
+    </div>
+  )
+}

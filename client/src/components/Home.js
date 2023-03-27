ea636eda16db59c0926d83cb4d870a4dd23491b0
rushiware <85820996+rushiware@users.noreply.github.com>
2023-03-27 19:18:44 +0530

    Add files via upload

---
+++ client/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='container mt-4 text-center'>
+      <h3>My University</h3>
+      <img src="https://tse1.mm.bing.net/th?id=OIP.Dr9qIEI6hk8_PN4lBpONVQHaE9&pid=Api&P=0" className='img img-fluid mt-2' style={{border: '2px solid black'}} alt="" />
+    </div>
+  )
+}
+
+export default Home
